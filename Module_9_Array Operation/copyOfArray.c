3356cef268f34ad4e2d442e981a229beb3bac045
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-27 19:19:46 +0600

    done 9.2 - Copy of an Array

--- Module_9_Array Operation/copyOfArray.c
+++ Module_9_Array Operation/copyOfArray.c
@@ -3,6 +3,52 @@ int main()
 {
 
     printf("hi\n");
+    printf("hello world\n");
+
+    // int a[];
+    // int a[] = {};
+    // int b[] = {};
+
+    int length1 = 0;
+    int length2 = 0;
+
+    int a[length1];
+    int b[length2];
+
+    // int length1 = 0;
+    // int length2 = 0;
+
+    scanf("%d", &length1);
+
+    for (int i = 0; i < length1; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < length1; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < length1; i++)
+    {
+        printf("%d ", b[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < length1; i++)
+    {
+        b[i] = a[i];
+    }
+
+    for (int i = 0; i < length1; i++)
+    {
+        printf("%d ", b[i]);
+    }
+    printf("\n");
 
     return 0;
 }
